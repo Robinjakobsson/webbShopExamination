f56dcfbd03aca9aad9fef56656d742c0c623d83a
Kseniia <anillinnittakorall@gmail.com>
2025-10-06 17:20:20 +0200

    Refactor PayPage component to improve payment form layout and add functionality for handling payment submission

--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../css/pay-page.css'
 
 const movies = [
@@ -7,63 +8,93 @@ const movies = [
 ]
 
 const PayPage = () => {
-    return (
-        <div className="pay-page">
-            <section className="payment-section">
-                <h1>Your order</h1>
-                <form className='pay-form'>
-                    <label>
-                        <span>Card number:</span>
-                        <input type="text" placeholder="1234 5678 9012 3456" required />
 
-                    </label>
-                    {/* hit kan man lägga till ikoner senare
+
+function backToLibrary() {
+    window.location.href = '#/library';
+}
+
+const [paid, setPaid] = useState(false);
+
+//doesn't let to proceed if some card fields are empty
+function handleSubmit(event) {
+   event.preventDefault();
+    setPaid(true);
+}
+
+
+    if (!paid) {
+        return (
+            <div className="pay-page">
+                <section className="payment-section">
+                    <h1>Your order</h1>
+                    <form className='pay-form'
+                        
+                       onSubmit={handleSubmit}
+                        
+                        >
+                        <label>
+                            <span>Card number:</span>
+                            <input type="text" placeholder="1234 5678 9012 3456" required />
+
+                        </label>
+                        {/* hit kan man lägga till ikoner senare
 <div className='card-icons'>
     </div>
 */}
 
-                    <div className="date-cvv-row">
-                        <label>
-                            <span>Date:</span>
-                            <input type="text" placeholder='MM/YY' required />
-                        </label>
+                        <div className="date-cvv-row">
+                            <label>
+                                <span>Date:</span>
+                                <input type="text" placeholder='MM/YY' required />
+                            </label>
 
-                        <label>
-                            <span>Security code:</span>
-                            <input type="text" placeholder='CVV' required />
-                        </label>
-                    </div>
-
-                    <label>
-                        <span>Card owner:</span>
-                        <input type="text" placeholder='Name as on the card' required />
-                    </label>
-
-                    <div className="remember-toggle">
-<span>Remember this card</span>
-<input type="checkbox" />
-                    </div>
-                    <button type="submit" className='pay-button'>Pay</button>
-
-                </form>
-            </section>
-
-            <section className="movie-info-section">
-                {movies.map((movie) => (
-                    <div key={movie.id} className="movie-item">
-                        <img
-                            src={movie.image}
-                            alt="no image" />
-                        <div className="movie-texts">
-                            <h2>{movie.title}</h2>
-                            <p>Price: {movie.price}kr.</p>
+                            <label>
+                                <span>Security code:</span>
+                                <input type="text" placeholder='CVV' required />
+                            </label>
                         </div>
-                    </div>
-                ))}
 
-            </section>
-        </div>
-    )
+                        <label>
+                            <span>Card owner:</span>
+                            <input type="text" placeholder='Name as on the card' required />
+                        </label>
+
+                        <div className="remember-toggle">
+                            <input type="checkbox" />
+                            <span> Remember this card</span>
+
+                        </div>
+                        <button type="submit" className='pay-button'>Pay</button>
+
+                    </form>
+                </section>
+
+                <section className="movie-info-section">
+                    {movies.map((movie) => (
+                        <div key={movie.id} className="movie-item">
+                            <img
+                                src={movie.image}
+                                alt="no image" />
+                            <div className="movie-texts">
+                                <h2>{movie.title}</h2>
+                                <p>Price: {movie.price}kr.</p>
+                            </div>
+                        </div>
+                    ))}
+
+                </section>
+            </div>
+        )
+    } else {
+        return (
+            <div className="payment-complete">
+                <h1>Thank you for your purchase!</h1>
+                <p>Your order number is {Math.floor(Math.random()*1000 +1)}.</p>
+                <button onClick={backToLibrary}>Back to Library</button>
+            </div>
+        )
+    }
 }
 
 export default PayPage
