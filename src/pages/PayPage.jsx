a662c68fbea1e05e0cd657306df511f368629212
Kseniia <anillinnittakorall@gmail.com>
2025-10-06 20:04:38 +0200

    Update PayPage component to enhance payment form functionality and layout, including total price display and improved styling.

--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -5,22 +5,26 @@ const movies = [
     { id: 1, title: 'Movie 1', price: 10, image: 'url1' },
     { id: 2, title: 'Movie 2', price: 15, image: 'url2' },
     { id: 3, title: 'Movie 3', price: 20, image: 'url3' },
+    { id: 4, title: 'Movie 4', price: 10, image: 'url1' },
+    { id: 5, title: 'Movie 5', price: 15, image: 'url2' },
+    { id: 6, title: 'Movie 6', price: 20, image: 'url3' },
+
 ]
 
 const PayPage = () => {
+    const totalPrice = movies.reduce((total, movie) => total + movie.price, 0);
 
+    function backToLibrary() {
+        window.location.href = '#/library';
+    }
 
-function backToLibrary() {
-    window.location.href = '#/library';
-}
+    const [paid, setPaid] = useState(false);
 
-const [paid, setPaid] = useState(false);
-
-//doesn't let to proceed if some card fields are empty
-function handleSubmit(event) {
-   event.preventDefault();
-    setPaid(true);
-}
+    //doesn't let to proceed if some card fields are empty
+    function handleSubmit(event) {
+        event.preventDefault();
+        setPaid(true);
+    }
 
 
     if (!paid) {
@@ -29,10 +33,10 @@ function handleSubmit(event) {
                 <section className="payment-section">
                     <h1>Your order</h1>
                     <form className='pay-form'
-                        
-                       onSubmit={handleSubmit}
-                        
-                        >
+
+                        onSubmit={handleSubmit}
+
+                    >
                         <label>
                             <span>Card number:</span>
                             <input type="text" placeholder="1234 5678 9012 3456" required />
@@ -65,7 +69,7 @@ function handleSubmit(event) {
                             <span> Remember this card</span>
 
                         </div>
-                        <button type="submit" className='pay-button'>Pay</button>
+                        <button type="submit" className='pay-button'>Pay {totalPrice}kr.</button>
 
                     </form>
                 </section>
@@ -90,7 +94,7 @@ function handleSubmit(event) {
         return (
             <div className="payment-complete">
                 <h1>Thank you for your purchase!</h1>
-                <p>Your order number is {Math.floor(Math.random()*1000 +1)}.</p>
+                <p>Your order number is {Math.floor(Math.random() * 1000 + 1)}.</p>
                 <button onClick={backToLibrary}>Back to Library</button>
             </div>
         )
