569bfb06b690793c93c8ee95de2c3c3c7ecd0a0c
Kseniia <anillinnittakorall@gmail.com>
2025-10-06 15:11:32 +0200

    Add payment form and movie display to PayPage component

--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -1,9 +1,61 @@
 import '../css/pay-page.css'
 
+const movies = [
+    {id: 1, title: 'Movie 1', price: 10, image: 'url1'},
+    {id: 2, title: 'Movie 2', price: 15, image: 'url2'},
+    {id: 3, title: 'Movie 3', price: 20, image: 'url3'},
+]
+
 const PayPage = () => {
     return (
-        <>
-        </>
+        <div className="pay-page">
+            <section className="payment-section">
+                <h1>Your order</h1>
+                <form className='pay-form'>
+                    <label>
+                        <span>Card number:</span>
+                        <input type="text" placeholder="1234 5678 9012 3456" required />
+                        
+                    </label>
+
+
+                    <div className="date-cvv-row">
+                        <label>
+                            <span>Date:</span>
+                            <input type="text" placeholder='MM/YY' required />
+                        </label>
+
+                        <label>
+                            <span>Security code:</span>
+                            <input type="text" placeholder='CVV' required />
+                        </label>
+                    </div>
+
+                    <label>
+                        <span>Card owner:</span>
+                        <input type="text" placeholder='Name as on the card' required />
+                    </label>
+
+                    <button type="submit" className='pay-button'>Pay</button>
+
+                </form>
+            </section>
+
+            <section className="movie-info-section">
+                {movies.map((movie) => (
+                    <div key={movie.id} className="movie-item">
+                        <img
+                            src={movie.image}
+                            alt="no image"/>
+                            <div className="movie-texts">
+                            <h2>{movie.title}</h2>
+                            <p>Price: {movie.price}kr.</p>
+                            </div>
+                    </div>
+                ))}
+              
+            </section>
+        </div>
     )
 }
 
