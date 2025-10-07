59385ca722e31643fbf8583487cc3fdd71a92b24
Simon-E-Mapp <3ggapp24.baugsi@folkuniversitetet.nu>
2025-10-07 14:24:52 +0200

    doing a fetch

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,10 +1,31 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faSquareGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router";
 
 const FooterComponent = () => {
 
     return(
         <footer className="mainFooter">
-            <p>Footer</p>
+
+            <Link className="mainFooterDeadLinks" to="/">
+            <p>Support</p></Link>
+            <Link className="mainFooterDeadLinks" to="/">
+            <p>Cookie-policy</p></Link>
+            <span className="mainFooterIcons">
+                <a href="https://www.instagram.com">
+                <FontAwesomeIcon icon={faInstagram}/>
+                </a>
+                <a href="https://www.facebook.com">
+                <FontAwesomeIcon icon={faFacebook}/>
+                </a>
+                <a href="https://x.com/">
+                <FontAwesomeIcon icon={faXTwitter}/>
+                </a>
+                <a href="https://github.com/Robinjakobsson/webbShopExamination/">
+                <FontAwesomeIcon icon={faSquareGithub}/>
+                </a>
+
+            </span>
         </footer>
     )
 }
