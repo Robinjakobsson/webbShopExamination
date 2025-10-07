4efdae2ed42eda999561c6de626d0f10d7209921
Robin Jakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-10-07 14:23:20 +0200

    Few tweaks, can now see movies more clearly in a grid format if they choose to

--- src/pages/GenreDetailPage.jsx
+++ src/pages/GenreDetailPage.jsx
@@ -5,17 +5,11 @@ import HorizontalListCard from '../components/HorizontalListCard';
 const GenreDetailPage = () => {
     const location = useLocation();
     const { movies } = location.state;
-
-
-
-
+    
     return (
         <>
 
-        <article>
-            
-
-        </article>
+        
         <section className='genreGrid'>
             {movies.map((movie) => (
                 <HorizontalListCard key={movie.id} movie={movie} />
