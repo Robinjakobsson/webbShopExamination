c45074a08a2a94515dd951fc43a35d6cc6d6b6d4
Kseniia <anillinnittakorall@gmail.com>
2025-10-09 13:39:32 +0200

    Add cleanCart action and integrate it into PayPage submission process

--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import '../css/pay-page.css'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { cleanCart } from '../features/cartSlice';
+
 
 const PayPage = () => {
+    const dispatch = useDispatch();
     const cartItems = useSelector((state) => state.cart.cartItems);
     console.log(cartItems)
     const imgUrl = "https://image.tmdb.org/t/p/w92"
@@ -18,83 +21,83 @@ const PayPage = () => {
     function handleSubmit(event) {
         event.preventDefault();
         setPaid(true);
+        dispatch(cleanCart());
     }
 
+        if (!paid) {
+            return (
+                <div className="pp-pay-page">
 
-    if (!paid) {
-        return (
-            <div className="pp-pay-page">
+                    <section className="pp-payment-section">
+                        <form className='pp-pay-form'
 
-                <section className="pp-payment-section">
-                    <form className='pp-pay-form'
+                            onSubmit={handleSubmit}
 
-                        onSubmit={handleSubmit}
+                        >
+                            <label>
+                                <span>Card number:</span>
+                                <input type="text" placeholder="1234 5678 9012 3456" required />
 
-                    >
-                        <label>
-                            <span>Card number:</span>
-                            <input type="text" placeholder="1234 5678 9012 3456" required />
-
-                        </label>
-                        {/* hit kan man lägga till ikoner senare
+                            </label>
+                            {/* hit kan man lägga till ikoner senare
 <div className='card-icons'>
     </div>
 */}
 
-                        <div className="pp-date-cvv-row">
-                            <label>
-                                <span>Date:</span>
-                                <input type="text" placeholder='MM/YY' required />
-                            </label>
+                            <div className="pp-date-cvv-row">
+                                <label>
+                                    <span>Date:</span>
+                                    <input type="text" placeholder='MM/YY' required />
+                                </label>
 
-                            <label>
-                                <span>Security code:</span>
-                                <input type="text" placeholder='CVV' required />
-                            </label>
-                        </div>
-
-                        <label>
-                            <span>Card owner:</span>
-                            <input type="text" placeholder='Name as on the card' required />
-                        </label>
-
-                        <div className="pp-remember-toggle">
-                            <input type="checkbox" />
-                            <span> Remember this card</span>
-
-                        </div>
-                        <button type="submit" className='pp-pay-button'>Pay {totalPrice}kr.</button>
-
-                    </form>
-                </section>
-
-                <section className="pp-movie-info-section">
-                    {cartItems.map((item) => (
-                        <div key={item.movie.id} className="pp-movie-item">
-                            <img
-                                src={`${imgUrl}${item.movie.poster_path}`}
-                                alt="no image" />
-                            <div className="pp-movie-texts">
-                                <h2>{item.movie.title.length > 20
-                                    ? item.movie.title.slice(0, 20) + "..."
-                                    : item.movie.title}</h2>
-                                <p>Price: {item.price * item.quantity} kr.</p>
+                                <label>
+                                    <span>Security code:</span>
+                                    <input type="text" placeholder='CVV' required />
+                                </label>
                             </div>
-                        </div>
-                    ))}
 
-                </section>
-            </div>
-        )
-    } else {
-        return (
-            <div className="pp-payment-complete">
-                <h1>Thank you for your purchase!</h1>
-                <p>Your order number is {Math.floor(Math.random() * 1000 + 1)}.</p>
-                <button onClick={backToLibrary}>Back to Library</button>
-            </div>
-        )
+                            <label>
+                                <span>Card owner:</span>
+                                <input type="text" placeholder='Name as on the card' required />
+                            </label>
+
+                            <div className="pp-remember-toggle">
+                                <input type="checkbox" />
+                                <span> Remember this card</span>
+
+                            </div>
+                            <button type="submit" className='pp-pay-button'>Pay {totalPrice}kr.</button>
+
+                        </form>
+                    </section>
+
+                    <section className="pp-movie-info-section">
+                        {cartItems.map((item) => (
+                            <div key={item.movie.id} className="pp-movie-item">
+                                <img
+                                    src={`${imgUrl}${item.movie.poster_path}`}
+                                    alt="no image" />
+                                <div className="pp-movie-texts">
+                                    <h2>{item.movie.title.length > 20
+                                        ? item.movie.title.slice(0, 20) + "..."
+                                        : item.movie.title}</h2>
+                                    <p>Price: {item.price * item.quantity} kr.</p>
+                                </div>
+                            </div>
+                        ))}
+
+                    </section>
+                </div>
+            )
+        } else {
+            return (
+                <div className="pp-payment-complete">
+                    <h1>Thank you for your purchase!</h1>
+                    <p>Your order number is {Math.floor(Math.random() * 1000 + 1)}.</p>
+                    <button onClick={backToLibrary}>Back to Library</button>
+                </div>
+            )
+        }
     }
-}
 
-export default PayPage
+    export default PayPage
