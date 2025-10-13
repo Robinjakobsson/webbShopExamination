f98f30ac3d9fe3eee1ab32ac2e55c1b280c474d3
Simon-E-Mapp <3ggapp24.baugsi@folkuniversitetet.nu>
2025-10-13 15:33:04 +0200

    Background done

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -3,6 +3,7 @@ import '../css/welcome-page.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react';
 import { fetchAllMovies } from '../features/moviesSlice';
+import Particles from '../components/BackgroundParticles';
 
 const WelcomePage = () => {
 
@@ -15,8 +16,21 @@ const WelcomePage = () => {
         
     return (
         <section className="welcomePage">
+            <div className="particles-wrapper">
+                <Particles
+                particleColors={['#FF00D9'/*magenta*/, '#04FF00'/*limegrön*/, '#00FFFB'/*isblå */]} // ändrar färgerna på partiklarna, kan lägga till fler
+                particleCount={500} //ändra antalet partiklar
+                particleSpread={5} //ändra tätheten på partiklarna
+                speed={0.1}
+                particleBaseSize={100}
+                moveParticlesOnHover={true} //pga. pointer-events: none; i CSSen så är fungerar inte denna raden.
+                alphaParticles={false}
+                disableRotation={false}
+                />
+            </div>
+            
+            <div className="welcomePageContent">
             <div className="welcomePageMovieRow">
-                
             {movies.popular?.slice(0, 4).map((movie) => (
                 <div key ={movie.id} className='welcomePageMovieCard'>
                     <Link to="/detail" state={{movie}}>
@@ -27,10 +41,11 @@ const WelcomePage = () => {
             ))}
             </div>
         <Link className="welcomePageBtn" to="/library">Library Page</Link>
-        <p className="welcomePageText">Welcome to your favorite Streamingsite!</p>
+        <p className="welcomePageText">Welcome to your favorite movie-site!</p>
         <p className="welcomePageText">Everything there is to watch, in just one place!</p>
+        </div>
         </section>
-    )
-}
+    );
+};
 
 export default WelcomePage
